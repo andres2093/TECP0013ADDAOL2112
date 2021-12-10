717cbbc70d58c5df0bb54d13d93864155cbcf1d8
Andres Ramirez <andres200493@gmail.com>
2021-12-09 20:39:58 -0600

    S3 -> R1 - E2

--- s3/s3.sql
+++ s3/s3.sql
@@ -1,3 +1,5 @@
+-- Ejemplo 1
+
 show keys from venta;
 
 -- Inner join
@@ -14,3 +16,46 @@ on e.id_puesto = p.id_puesto;
 select * from empleado e
 right join puesto p
 on e.id_puesto = p.id_puesto;
+
+-- Reto 1
+-- ¿Cuál es el nombre de los empleados que realizaron cada venta?
+SELECT clave, nombre, apellido_paterno
+FROM venta v
+JOIN empleado e
+  ON v.id_empleado = e.id_empleado
+ORDER BY clave;
+-- ¿Cuál es el nombre de los artículos que se han vendido?
+SELECT clave, nombre
+FROM venta v
+JOIN articulo a
+  ON v.id_articulo = a.id_articulo
+ORDER BY clave;
+-- ¿Cuál es el total de cada venta?
+-- SELECT clave, round(sum(precio) + sum(iva), 2) as totalConIva 
+SELECT clave, round(sum(precio),2) AS total
+FROM venta v
+JOIN articulo a
+  ON v.id_articulo = a.id_articulo
+GROUP BY clave
+ORDER BY clave;
+
+-- Ejemplo 2
+SELECT v.clave, v.fecha, a.nombre as producto, a.precio, 
+concat(e.nombre, ' ', e.apellido_paterno, ' ', e.apellido_materno) as empleado 
+FROM venta v
+JOIN empleado e
+  ON v.id_empleado = e.id_empleado
+JOIN articulo a
+  ON v.id_articulo = a.id_articulo;
+  
+-- Ventas totales por empleado
+SELECT sum(a.precio) as total, 
+concat(e.nombre, ' ', e.apellido_paterno, ' ', e.apellido_materno) as empleado 
+FROM venta v
+JOIN empleado e
+  ON v.id_empleado = e.id_empleado
+JOIN articulo a
+  ON v.id_articulo = a.id_articulo
+group by v.id_empleado
+order by total desc;
+
